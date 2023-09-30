b2f66b0dc9a4af8db0fb17a768e074498b3d2e2b
Lion_Snail <116630921+LionSnail@users.noreply.github.com>
2023-09-30 12:37:28 +0300

    save

--- Main/Player/spaceship/machines.gd
+++ Main/Player/spaceship/machines.gd
@@ -1,2 +1,29 @@
 extends Area2D
 
+
+var is_mouse = false
+var active = false
+var type = name
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("click") and is_mouse:
+		active = true
+	
+	if active:
+		get_parent().get_parent().active_current(name)
+
+
+
+
+
+func highlight(enabled):
+	pass
+
+func _on_mouse_entered():
+	is_mouse = true
+	highlight(true)
+
+
+func _on_mouse_exited():
+	is_mouse = false
+	highlight(false)
--- Main/Player/spaceship/spaceship.gd
+++ Main/Player/spaceship/spaceship.gd
@@ -1,3 +1,33 @@
 extends Node2D
 
 @onready var vieport_size 
+
+var oxy_amount = 100
+var engine_amount = 100
+var repair_amount = 100
+var current = ""
+
+func _ready():
+	current = "Oxygen"
+
+func _physics_process(delta):
+	match current:
+		"Oxygen":
+			oxy_amount += 1 * delta
+			oxy_amount = clamp(oxy_amount, 0, 100)
+		
+		"Engine":
+			engine_amount += 1 * delta
+			engine_amount = clamp(engine_amount, 0, 100)
+			
+		"Repair":
+			repair_amount += 1 * delta
+			engine_amount = clamp(engine_amount, 0, 100)
+			
+		"Guns":
+			pass
+	print(current, oxy_amount)
+
+func get_current(type):
+	current = type
+
