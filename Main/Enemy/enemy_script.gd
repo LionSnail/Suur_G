32c0da496ec3e87dab673bc3c54dc3d7ac20fa1e
Lion_Snail <116630921+LionSnail@users.noreply.github.com>
2023-09-30 12:12:59 +0300

    save

--- Main/Enemy/enemy_script.gd
+++ Main/Enemy/enemy_script.gd
@@ -1,4 +1,37 @@
 extends Area2D
 
-var player
-var stats
+var is_mouse = false
+@export var stats : Resource
+
+var hp
+var speed
+var idle_sprite
+var latch_sprite
+
+var dir_x = 0
+
+func _ready():
+	hp = stats.hp
+	speed = stats.speed
+	idle_sprite = stats.sprite_idle
+	latch_sprite = stats.sprite_latch
+	
+
+
+func _physics_process(delta):
+	move_local_x(dir_x)
+	
+	if Input.is_action_just_pressed("click") and is_mouse:
+		hp -= 1
+		if hp <= 0:
+			queue_free()
+
+
+
+
+func _on_mouse_entered():
+	is_mouse = true
+
+
+func _on_mouse_exited():
+	is_mouse = false
--- Main/Enemy/enemy_spawner_script.gd
+++ Main/Enemy/enemy_spawner_script.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
+@export var dir_x : int = 0
 
 @onready var enemy_base = load("res://Main/Enemy/enemy.tscn")
-@onready var enemy_types = {
-	
+@onready var enemy_type = {
+	1 : load("res://Main/Enemy/resources/Enemy_basic.tres")
 }
 var is_emitting = false
 
-func create_enemy():
-	pass
+func create_enemy(type):
+	var enemy_i = enemy_base.instantiate()
+	enemy_i.position.y = randi_range(-250, 250)
+	enemy_i.position.x = self.position.x
+	enemy_i.stats = type
+	enemy_i.dir_x = dir_x
+	get_tree().get_root().get_node("Main/Enemies").add_child(enemy_i)
 
 func _on_timer_timeout():
-	pass
+	randomize()
+	var randnum = randi_range(1, 1)
+	var type = enemy_type[randnum]
+	create_enemy(type)
