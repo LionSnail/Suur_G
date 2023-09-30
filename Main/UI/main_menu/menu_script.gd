40c23a744ddf95ee23b588d995bb3208c608c423
Lion_Snail <116630921+LionSnail@users.noreply.github.com>
2023-09-30 13:54:56 +0300

    added quit

--- Main/UI/main_menu/menu_script.gd
+++ Main/UI/main_menu/menu_script.gd
@@ -4,3 +4,12 @@ var opening_scene = load("res://Main/main.tscn.tscn")
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Main/main.tscn")
+
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
