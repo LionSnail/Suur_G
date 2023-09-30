eb1a24ef63cbc9235357f40bc686d9f6e7a33c9c
Lion_Snail <116630921+LionSnail@users.noreply.github.com>
2023-09-30 16:46:10 +0300

    alien animations

--- Main/Enemy/enemy_script.gd
+++ Main/Enemy/enemy_script.gd
@@ -17,10 +17,17 @@ func _ready():
 	idle_sprite = stats.sprite_idle
 	latch_sprite = stats.sprite_latch
 	
+	$Sprite2D.play("default")
+	
 	if dir_x == -1:
 		$Sprite2D.flip_h = false
 		$CollisionPolygon2D.scale.x = -1
-	
+
+
+func die():
+#	$AnimationPlayer.play("death")
+	await get_tree().create_timer(1).timeout
+	queue_free()
 
 
 func _physics_process(delta):
@@ -29,7 +36,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("click") and is_mouse:
 		hp -= 1
 		if hp <= 0:
-			queue_free()
+			die()
+
+
 
 
 func latch():
