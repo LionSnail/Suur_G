c4295730430a871f9a2352bafc6663c75200161f
Timur Nizamov <ruclan99999@mail.ru>
2023-10-01 11:44:47 +0300

    banks

---
+++ Main/Player/spaceship/clickSounds.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var engine_click = ""
+@export var oxygen_click = ""
+@export var spaceship_collide = ""
+@export var enemy_death = ""
+@export var ui_click = ""
+
+func _ready():
+	pass
+
+func request_sound(type):
+	pass
